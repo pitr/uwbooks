572343a0ff2c6bc7680b5f8c58570de9ff78dc04
pitr <pitr.vern@gmail.com>
2010-11-30 08:48:03 -0500

    fix auto-pagination when there are no more pages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,26 +1,28 @@
+var scrollLock = false;
+var page = 1;
+
 $(document).ready(function() {
 
+  // notice wrapper click
   $('#notice_wrapper').live('click', function() {
     $(this).slideUp();
     return false;
   });
 
+  // book actions mouseover
   $('.book').live('mouseenter', function() {
     $(this).find('.book_actions').fadeIn(100);
   }).live('mouseleave', function() {
     $(this).find('.book_actions').fadeOut(100);
   });
 
-  var scrollLock = false;
-  var page = 1;
+  // scroll pagination
   $(window).scroll(function() {
-    $('#footer').html(($(document).height()-$(window).height())-$(window).scrollTop());
-    if(!scrollLock && ($(document).height()-$(window).height())-$(window).scrollTop()<=300) {
+    if (!scrollLock && ($(document).height()-$(window).height())-$(window).scrollTop()<=300) {
       scrollLock = true;
       page += 1;
       $('#pagination_wrapper').fadeIn();
       $.getScript('?page='+page, function() {
-        scrollLock = false;
         $('#pagination_wrapper').fadeOut();
       });
     }
