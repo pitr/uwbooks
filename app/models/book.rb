f682e43bd486bce36a57eba13374fe3e570d9b28
pitr <pitr.vern@gmail.com>
2010-11-30 22:47:52 -0500

    add validation for books (minus ISBN)

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,16 @@
 class Book < ActiveRecord::Base
   belongs_to :user
 
+  validates_numericality_of :price
   validates_presence_of :author
+  validates_presence_of :title
 
   validate :isbn_is_valid
 
   default_scope order('created_at DESC').where(:removed => false)
 
   def isbn_is_valid
+    # TODO: actually check
     true
   end
 
