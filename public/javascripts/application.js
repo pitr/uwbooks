36573ae795703e076346f081c7a6df8f248c6ec6
pitr <pitr.vern@gmail.com>
2010-12-03 22:11:30 -0500

    make scroller into a jQuery plugin

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,33 @@
-var scrollLock = false;
-var page = 1;
+(function( $ ){
+  var page = 1;
+
+  var methods = {
+    startScroller: function() {
+      $(window).bind('scroll.scroller', function() {
+        if (($(document).height()-$(window).height())-$(window).scrollTop() <= 300) {
+          $.scroller({stop:true});
+          page += 1;
+          $('#pagination_wrapper').fadeIn();
+          $.getScript('?page='+page, function() {
+            $('#pagination_wrapper').fadeOut();
+            $.scroller();
+          });
+        }
+      });
+    },
+    stopScroller: function() {
+      $(window).unbind('.scroller');
+    }
+  };
+
+  $.scroller = function(options) {
+    if(options && options.stop) {
+      methods.stopScroller();
+    } else {
+      methods.startScroller();
+    }
+  };
+})( jQuery );
 
 $(document).ready(function() {
 
@@ -14,18 +42,6 @@ $(document).ready(function() {
     $(this).find('.book_actions').fadeIn(100);
   }).live('mouseleave', function() {
     $(this).find('.book_actions').fadeOut(100);
-  });
-
-  // scroll pagination
-  $(window).scroll(function() {
-    if (!scrollLock && ($(document).height()-$(window).height())-$(window).scrollTop()<=300) {
-      scrollLock = true;
-      page += 1;
-      $('#pagination_wrapper').fadeIn();
-      $.getScript('?page='+page, function() {
-        $('#pagination_wrapper').fadeOut();
-      });
-    }
   });
 
 });
